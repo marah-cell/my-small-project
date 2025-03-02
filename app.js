fa6f05f10c5b137e98445dcf78503d1506e829b9
marah <marahsaleh08@gmail.com>
2025-03-02 03:47:38 +0300

    app.js plus test.js

---
+++ app.js
@@ -0,0 +1,2 @@
+// app.js
+console.log("Hello, this is my small project!");
---
+++ test.js
@@ -0,0 +1,8 @@
+// test.js
+const assert = require('assert');
+
+describe('Simple Test', () => {
+  it('should return true', () => {
+    assert.strictEqual(true, true);
+  });
+});
